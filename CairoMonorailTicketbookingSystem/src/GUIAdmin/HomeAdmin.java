845be6dcf003dd6ba5aa7e92fd5794f204752c46
Mahmoud Osama <mhmudos24@gmail.com>
2021-05-29 20:55:01 +0200

    Admin GUI

--- CairoMonorailTicketbookingSystem/src/GUIAdmin/HomeAdmin.java
+++ CairoMonorailTicketbookingSystem/src/GUIAdmin/HomeAdmin.java
@@ -4,16 +4,27 @@
  */
 package GUIAdmin;
 
+import Person.Admin;
+import GUIAdmin.*;
+import GUIGeneral.UserType;
+import Station.*;
 /**
  *
  * @author Ash
  */
 public class HomeAdmin extends javax.swing.JFrame {
 
+    Admin currentUser = null;
     /**
      * Creates new form HomeAdmin
      */
     public HomeAdmin() {
+        initComponents();
+    }
+
+    public HomeAdmin(Admin a) {
+        jLabel2.setText(a.getName());
+        currentUser = a;
         initComponents();
     }
 
@@ -26,21 +37,139 @@ public class HomeAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        ModifyAccount = new javax.swing.JButton();
+        GenerateReport = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        Train = new javax.swing.JButton();
+        Logout = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
+        jLabel1.setText("Welcome");
+
+        jLabel2.setFont(new java.awt.Font("Lucida Grande", 1, 24)); // NOI18N
+        jLabel2.setText("jLabel2");
+
+        jLabel3.setFont(new java.awt.Font("Lucida Grande", 0, 24)); // NOI18N
+        jLabel3.setText("To the admin panel");
+
+        ModifyAccount.setText("Modify account");
+        ModifyAccount.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ModifyAccountActionPerformed(evt);
+            }
+        });
+
+        GenerateReport.setText("Generate reports");
+        GenerateReport.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                GenerateReportActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Lucida Grande", 0, 18)); // NOI18N
+        jLabel4.setText("choose the action you would like to perform");
+
+        Train.setText("Add/Modify train");
+        Train.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TrainActionPerformed(evt);
+            }
+        });
+
+        Logout.setText("Logout");
+        Logout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                LogoutActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(88, 88, 88)
+                                .addComponent(ModifyAccount)
+                                .addGap(60, 60, 60)
+                                .addComponent(Logout))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(64, 64, 64)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(GenerateReport)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(Train))
+                            .addComponent(jLabel4))))
+                .addContainerGap(76, Short.MAX_VALUE))
+            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addGap(26, 26, 26)
+                .addComponent(jLabel4)
+                .addGap(52, 52, 52)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(GenerateReport)
+                    .addComponent(Train))
+                .addGap(28, 28, 28)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Logout)
+                    .addComponent(ModifyAccount))
+                .addContainerGap(84, Short.MAX_VALUE))
         );
+
+        jLabel2.getAccessibleContext().setAccessibleName("adminName");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ModifyAccountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ModifyAccountActionPerformed
+        ModifyAccount mod = new ModifyAccount(currentUser);
+        mod.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_ModifyAccountActionPerformed
+
+    private void GenerateReportActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_GenerateReportActionPerformed
+        GenerateReports gen = new GenerateReports(currentUser);
+        gen.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_GenerateReportActionPerformed
+
+    private void TrainActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TrainActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TrainActionPerformed
+
+    private void LogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogoutActionPerformed
+        // TODO add your handling code here:
+        UserType logout = new UserType();
+        logout.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_LogoutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +207,14 @@ public class HomeAdmin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton GenerateReport;
+    private javax.swing.JButton Logout;
+    private javax.swing.JButton ModifyAccount;
+    private javax.swing.JButton Train;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
